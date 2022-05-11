b7d83b55f88fb8f4f8157197a1fadb9412a6b8fa
heron03 <heronze@gmail.com>
2022-05-10 21:05:13 -0300

    Api js

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Enviar no Postman
 {
     "query": "query { cep(message: \"17522-420\") }"
 }
+
+Heron 588652
+Joyce 588083
+Matheus Melo 591203
---
+++ api.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch'); //npm install node-fetch npm i node-fetch@2.6.1
+var express = require('express'); //npm install express express-graphql graphql --save --force
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+ 
+var schema = buildSchema(`
+  type CEP {
+    logradouro: String
+    bairro: String
+    localidade: String
+    uf: String
+  }
+ 
+  type Query {
+    consultaCep(cep: String!): CEP
+  }
+`);
+ 
+var root = {
+consultaCep: async ({cep}) => {
+    let response = await fetch("https://viacep.com.br/ws/"+cep+"/json/")
+    if (response.status === 200) {
+      return response.json()
+    } else {
+      throw new Error('Something bad happened :(')
+    }    
+}
+};
+ 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);,
